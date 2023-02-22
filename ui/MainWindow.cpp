e2fcc806bc8569352821ba0600495b3048e79372
Владислав Тараканов <10994434-lazyDesertMan@users.noreply.gitlab.com>
2023-02-22 15:14:58 +0400

    Исправлена проблема с именами файлов

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -1,13 +1,13 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->splitter->setStretchFactor(0,1);
-    ui->splitter->setStretchFactor(1,0);
+    ui->splitter->setStretchFactor(0, 1);
+    ui->splitter->setStretchFactor(1, 10);
 }
 
 MainWindow::~MainWindow()
