396a9a669ea5aa13ec5572906a967bda8fe84188
Владислав Тараканов <10994434-lazyDesertMan@users.noreply.gitlab.com>
2023-02-22 18:47:07 +0400

    Обработаны запуски для теста логики и визуала

--- main.cpp
+++ main.cpp
@@ -6,34 +6,28 @@
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    /*QJSEngine engine;
+    bool isTesing = false;
 
-    MivarModel model = ModelLoader::load("./files/proj3.xml");
-    std::cout << "ID:        " << model.id().toStdString() << std::endl
-              << "Имя:       " << model.name().toStdString() << std::endl
-              << "Описание:  " << model.description().toStdString() << std::endl
-              << "Отношения: " << std::endl;
-    for (auto r : model.relations()) {
-        std::cout << "\tID:       " << r.id().toStdString() << std::endl
-                  << "\tИмя:      " << r.name().toStdString() << std::endl
-                  << "\tТип:      " << r.type().toStdString() << std::endl
-                  << "\tОписание: " << r.description().toStdString() << std::endl
-                  << "\tВход:     " << std::endl;
-        for(auto param : r.inputs())
-            std::cout << "\t\t" << param.name().toStdString() << " : " << param.type().toStdString() << std::endl;
-        std::cout << "\tВыход:    " << std::endl;
-        for(auto param : r.outputs())
-            std::cout << "\t\t" << param.name().toStdString() << " : " << param.type().toStdString() << std::endl;
-        std::cout << "\tКод:      " << r.code().toStdString() << std::endl << std::endl;
+    for (int i = 0; i < argc; i++) {
+        if (QString::fromStdString(argv[i]) == "-D")
+            isTesing = true;
     }
-    std::shared_ptr<MivarRelation> rel = model.relations()[1];
-    std::cout << rel->name().toStdString() << std::endl;
-    QJSValue func = engine.evaluate(rel->toJSFunction());
-    QJSValueList params;
-    params << 1 << 2 << 5;
-    QJSValue res = func.call(params);
-    std::cout << res.toString().toStdString() << std::endl;*/
-    MainWindow container;
-    container.show();
-    app.exec();
+    
+    if (!isTesing) {
+        MainWindow container;
+        container.show();
+        app.exec();
+    }
+    else {
+        QJSEngine engine;
+
+        MivarModel model = ModelLoader::load("./files/proj3.xml");
+        std::shared_ptr<MivarRelation> rel = model.relations()[1];
+        std::cout << rel->name().toStdString() << std::endl;
+        QJSValue func = engine.evaluate(rel->toJSFunction());
+        QJSValueList params;
+        params << 1 << 2 << 5;
+        QJSValue res = func.call(params);
+        std::cout << res.toString().toStdString() << std::endl;
+    }
 }
