ecca4cdd394a5b00f365d571ce77dc94936358e0
AminASTU <irgaliev01@mail.ru>
2023-02-27 17:50:45 +0400

    mainwindow

--- ui/MainWindow.cpp
+++ ui/MainWindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->splitter->setStretchFactor(0, 1);
     ui->splitter->setStretchFactor(1, 10);
+    ui->displayTree->resize(250, 50);
 }
 
 MainWindow::~MainWindow()
