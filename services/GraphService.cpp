8f75196f987db6a9cf4d27ed2e31e4f473b737e7
Владислав Тараканов <vladislav.tarakanov@bk.ru>
2023-03-15 22:14:48 +0400

    Подготовка к обработке графов

---
+++ services/GraphService.cpp
@@ -0,0 +1,92 @@
+#include <boost/graph/tiernan_all_cycles.hpp>
+#include "GraphService.h"
+
+// Добавление пустой реализации для функции, требуемой в Boost graph_concepts.hpp, стр.459
+namespace boost { void renumber_vertex_indices(const Graph&) {} }
+
+QString GraphService::genNodeName(std::shared_ptr<MivarObject> parent, std::shared_ptr<MivarObject> child) {
+    QString parentName = parent->name();
+    QString childName = child->name();
+    return (parentName.remove("\"").remove("\n").remove("\r").replace(".", " ")) + "." + (childName.remove("\"").remove("\n").remove("\r").replace(".", " "));
+}
+
+void GraphService::loadParams(std::map<QString, size_t> &paramIndexList, std::shared_ptr<MivarClass> &mivarClass)
+{
+    for(std::shared_ptr<MivarParam> param : mivarClass->params())
+        paramIndexList.insert({param->id(), paramIndexList.size()});
+    for(std::shared_ptr<MivarClass> subclass : mivarClass->subclasses())
+        loadParams(paramIndexList, subclass);
+}
+
+void GraphService::loadParams(std::map<QString, std::pair<QString, QString>>& paramList, std::shared_ptr<MivarClass>& mivarClass) {
+    for(std::shared_ptr<MivarParam> param : mivarClass->params())
+        paramList.insert({param->id(), {genNodeName(mivarClass, param), ""}});
+    for(std::shared_ptr<MivarClass> subclass : mivarClass->subclasses())
+        loadParams(paramList, subclass);
+}
+
+void GraphService::loadRules(std::map<QString, size_t>& ruleIndexList, std::shared_ptr<MivarClass>& mivarClass) {
+    for(std::shared_ptr<MivarRule> rule : mivarClass->rules())
+        ruleIndexList.insert({rule->id(), ruleIndexList.size()});
+    for(std::shared_ptr<MivarClass> subclass : mivarClass->subclasses())
+        loadRules(ruleIndexList, subclass);
+}
+
+void GraphService::loadRules(std::map<QString, std::pair<QString, QString>>& paramList, std::map<QString, std::pair<QString, QString>>& ruleList, std::shared_ptr<MivarClass>& mivarClass) {
+    for(std::shared_ptr<MivarRule> rule : mivarClass->rules()) {
+        QString inputs = "[";
+        for(QString paramId : rule->inputsId()) {
+            inputs += "\"" + paramList[paramId].first + "\",";
+        }
+        inputs += "]";
+        const QString ruleName = genNodeName(mivarClass, rule);
+        for(QString paramId : rule->outputsId()) {
+            paramList[paramId].second += "\"" + ruleName + "\",";
+        }
+        ruleList.insert({rule->id(), {ruleName, inputs}});
+    }
+    for(std::shared_ptr<MivarClass> subclass : mivarClass->subclasses())
+        loadRules(paramList, ruleList, subclass);
+}
+
+void GraphService::bindParams(std::map<QString, size_t>& paramIndexList, std::map<QString, size_t>& ruleIndexList, Graph& graph, std::shared_ptr<MivarClass>& mivarClass) {
+    size_t ruleDisplace =  paramIndexList.size();
+    for(std::shared_ptr<MivarRule> rule : mivarClass->rules()) {
+        for(QString paramId : rule->inputsId())
+            boost::add_edge(paramIndexList[paramId], ruleIndexList[rule->id()] + ruleDisplace, graph);
+        for(QString paramId : rule->outputsId())
+            boost::add_edge(ruleIndexList[rule->id()] + ruleDisplace, paramIndexList[paramId], graph);
+    }
+    for(std::shared_ptr<MivarClass> subclass : mivarClass->subclasses())
+        bindParams(paramIndexList, ruleIndexList, graph, subclass);
+}
+
+std::vector<CycleDetails> GraphService::findRuleCycles(std::shared_ptr<MivarModel>& model) {
+    std::vector<CycleDetails> pathes;
+    std::map<QString, size_t> params;
+    std::map<QString, size_t> rules;
+    loadParams(params, model->modelClass());
+    loadRules(rules, model->modelClass());
+    Graph gr(params.size() + rules.size());
+    bindParams(params, rules, gr, model->modelClass());
+    CycleFinder finder(params.size(), rules, pathes);
+    tiernan_all_cycles(gr, finder);
+    return pathes;
+}
+
+QString GraphService::ruleDependencyJson(std::shared_ptr<MivarModel>& model) {
+    if (model.get() != nullptr) {
+        QString code = "[";
+        std::map<QString, std::pair<QString, QString>> paramDependecy;
+        std::map<QString, std::pair<QString, QString>> ruleDependecy;
+        loadParams(paramDependecy, model->modelClass());
+        loadRules(paramDependecy, ruleDependecy, model->modelClass());
+        for(auto it = paramDependecy.begin(); it != paramDependecy.end(); it++)
+            code += "{\"name\":\"" + it->second.first + "\",\"imports\":[" + it->second.second + "]},";
+        for(auto it = ruleDependecy.begin(); it != ruleDependecy.end(); it++)
+            code += "{\"name\":\"" + it->second.first + "\",\"imports\":" + it->second.second + "},";
+        code += "]";
+        return code;
+    }
+    return "[{\"name\":\"no model\"}]";
+}
---
+++ services/GraphService.h
@@ -0,0 +1,62 @@
+#ifndef GRAPH_SERVICE_H
+#define GRAPH_SERVICE_H
+
+#include <vector>
+#include <QString>
+#include <QDebug>
+#include <boost/graph/adjacency_list.hpp>
+#include <models/MivarModel.h>
+
+struct CycleDetails {
+    std::vector<QString> way;
+};
+
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS>;
+
+class GraphService {
+    struct CycleFinder {
+        CycleFinder(size_t params_cnt, std::map<QString, size_t>& rules, std::vector<CycleDetails>& pathes) : params_cnt(params_cnt), rules(rules), pathes(pathes) {}
+        std::vector<CycleDetails>& pathes;
+        size_t params_cnt;
+        std::map<QString, size_t>& rules;
+
+        template <typename Path>
+        void cycle(const Path& path, Graph const& g) {
+            CycleDetails cyclePath;
+            auto indices = get(boost::vertex_index, g);
+            for (auto node : path) {
+                auto idx = get(indices, node);
+                if (idx >= params_cnt) {
+                    idx -= params_cnt;
+                    for (auto rule : rules) 
+                        if (rule.second == idx)
+                            cyclePath.way.push_back(rule.first);
+                }
+            }
+            if(cyclePath.way.size())
+                pathes.push_back(cyclePath);
+        };
+    };
+
+    static QString genNodeName(std::shared_ptr<MivarObject> parent, std::shared_ptr<MivarObject> child);
+    static void loadParams(std::map<QString, size_t>& paramIndexList, std::shared_ptr<MivarClass>& mivarClass);
+    static void loadParams(std::map<QString, std::pair<QString, QString>>& paramList, std::shared_ptr<MivarClass>& mivarClass);
+    static void loadRules(std::map<QString, size_t>& ruleIndexList, std::shared_ptr<MivarClass>& mivarClass);
+    static void loadRules(std::map<QString, std::pair<QString, QString>>& paramList, std::map<QString, std::pair<QString, QString>>& ruleList, std::shared_ptr<MivarClass>& mivarClass);
+    /**
+     * @brief Добавление путей между графами и параметрами
+     * 
+     * @warning Первыми считаются узлы параметров, потом следуют узлы правил
+     * 
+     * @param paramIndexList Список параметров
+     * @param ruleIndexList Список правил
+     * @param graph Граф
+     * @param mivarClass Класс 
+     */
+    static void bindParams(std::map<QString, size_t>& paramIndexList, std::map<QString, size_t>& ruleIndexList, Graph& graph, std::shared_ptr<MivarClass>& mivarClass);
+public:
+    static std::vector<CycleDetails> findRuleCycles(std::shared_ptr<MivarModel>& model);
+    static QString ruleDependencyJson(std::shared_ptr<MivarModel>& model);
+};
+
+#endif
